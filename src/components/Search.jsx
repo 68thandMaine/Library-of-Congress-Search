88e9b82ab40883c4075fa1e8bb8cd4e42e6804b1
Student <example@example.com>
2019-03-04 09:32:12 -0800

    add input to search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
 import styles from './Search.css';
 
-function Search() {
-  return (
-    <div className={styles.search}>
-      <input type='text' />
-    </div>
-  );
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log('submit', this.state.value);
+  }
+
+  render() {
+    return (
+      <div className={styles.search}>
+        <form onSubmit={this.handleSubmit}>
+          <input type='text' value={this.state.value} onChange={this.handleChange} />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Search;
